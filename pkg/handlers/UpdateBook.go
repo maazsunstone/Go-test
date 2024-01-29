a22f3a1b442b5a67bc13be65185364c6f3f1baab
maazsunstone <jerrymaaz@gmail.com>
2024-01-29 17:56:23 +0530

    update route for the crud api

--- pkg/handlers/UpdateBook.go
+++ pkg/handlers/UpdateBook.go
@@ -3,18 +3,37 @@ package handlers
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"strconv"
 
 	"github.com/gorilla/mux"
 	"main.go/pkg/mocks"
+	"main.go/pkg/models"
 )
 
 func UpdateBook(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	id, _ := strconv.Atoi(vars["id"])
 	fmt.Println(id)
-	w.Header().Add("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(mocks.Book)
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		panic(err)
+	}
+	var updatedBookVars models.Book
+	json.Unmarshal(body, &updatedBookVars)
+	for index, i := range mocks.Book {
+		if i.Id == id {
+			i.Title = updatedBookVars.Title
+			i.Author = updatedBookVars.Author
+			i.Desc = updatedBookVars.Desc
+			mocks.Book[index] = i
+			w.Header().Add("Content-Type", "application/json")
+			w.WriteHeader(http.StatusOK)
+			json.NewEncoder(w).Encode("Updated")
+			break
+
+		}
+	}
+
 }
